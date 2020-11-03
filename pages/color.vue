424223874b1ce560828ae490e3ad7d41a405d876
NandeeshG <nandeeshgupta@gmail.com>
2020-11-03 20:55:23 +0530

    Added copy to clipboard and Hex colors in colorExtraction

--- pages/color.vue
+++ pages/color.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-container>
       <v-row justify="center">
-        <v-col cols="6" sm="6">
+        <v-col cols="12" sm="8">
           <img
             id="myimg"
             :src="source"
@@ -11,21 +11,36 @@
             crossorigin="anonymous"
           />
         </v-col>
-        <v-col cols="4" sm="4" align-self="center">
+        <v-col cols="12" sm="4" align-self="center">
           <v-row>
             <v-btn @click="reload"> NEW PICTURE </v-btn>
           </v-row>
           <v-row>
             <h1>PRIMARY COLOR</h1>
-            <v-btn :color="primary" block>{{ primary }}</v-btn>
+            <v-btn :color="primary" block @click="copyColor(primary)">{{
+              primary
+            }}</v-btn>
           </v-row>
         </v-col>
       </v-row>
       <h1>COLOR PALLETE</h1>
       <div v-for="(c, id) in colors" :key="id">
-        <v-btn :color="c.color" block>{{ c.color }}</v-btn>
+        <v-btn :color="c.color" block @click="copyColor(c.color)">{{
+          c.color
+        }}</v-btn>
       </div>
     </v-container>
+    <v-snackbar
+      v-model="successChip"
+      timeout="1000"
+      bottom
+      left
+      rounded="r-xl"
+      transition="slide-x-transition"
+      color="blue"
+    >
+      Copied {{ successChipText }}
+    </v-snackbar>
   </v-app>
 </template>
 
@@ -38,6 +53,8 @@ export default {
       colors: [],
       source: 'https://picsum.photos/400',
       primary: '',
+      successChip: false,
+      successChipText: '',
     }
   },
   mounted() {
@@ -54,11 +71,17 @@ export default {
 
       for (let i = 0; i < this.colors.length; ++i) {
         this.colors[i].value = this.colors[i]
-        this.colors[
-          i
-        ].color = `rgb(${this.colors[i].value[0]},${this.colors[i].value[1]},${this.colors[i].value[2]})`
+        this.colors[i].color = this.rgbToHex(
+          this.colors[i].value[0],
+          this.colors[i].value[1],
+          this.colors[i].value[2]
+        )
       }
-      this.primary = `rgb(${this.primary[0]},${this.primary[1]},${this.primary[2]})`
+      this.primary = this.rgbToHex(
+        this.primary[0],
+        this.primary[1],
+        this.primary[1]
+      )
     },
     loadImage(url, elem) {
       return new Promise((resolve, reject) => {
@@ -69,6 +92,26 @@ export default {
     },
     reload() {
       window.location.reload()
+    },
+    rgbToHex(r, g, b) {
+      return (
+        '#' +
+        [r, g, b]
+          .map((x) => {
+            const hex = x.toString(16)
+            return hex.length === 1 ? '0' + hex : hex
+          })
+          .join('')
+      )
+    },
+    copyColor(val) {
+      navigator.clipboard
+        .writeText(val)
+        .then(() => {
+          this.successChip = true
+          this.successChipText = val
+        })
+        .catch((err) => console.log(err))
     },
   },
 }
